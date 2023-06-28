85e63be3f26dcc4a89bd45f8a17ef18fde1b9e93
AmineC95 <amine.chioukh95@gmail.com>
2023-06-28 21:49:35 +0200

    init

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,110 @@
+import scalafx.application.JFXApp3.PrimaryStage
+import scalafx.application.{JFXApp3, Platform}
+import scalafx.beans.property.ObjectProperty
+import scalafx.scene.Scene
+import javafx.scene.input.KeyCode
+import scalafx.scene.paint.Color._
+import scalafx.scene.shape.Rectangle
+import scalafx.scene.SceneIncludes.jfxScene2sfx
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import scala.util.Random
+
+object Main extends JFXApp3 {
+  val windowSize = 600
+  val cellSize = 25
+
+  type Coordinates = (Int, Int)
+  case class Predator(location: Coordinates)
+  case class Prey(location: Coordinates)
+  case class State(predators: List[Predator], prey: Prey)
+
+  override def start(): Unit = {
+    val initialState = ObjectProperty(
+      State(List(Predator((250, 250))), Prey((300, 300)))
+    )
+
+    stage = new PrimaryStage {
+      title = "Predator Prey Game"
+      width = windowSize
+      height = windowSize
+      scene = new Scene {
+        fill = LightBlue
+        content = drawState(initialState.value)
+
+        onKeyPressed = { key =>
+          val currentState = initialState.value
+          key.getCode match {
+            case KeyCode.UP => initialState.value = currentState.copy(prey = Prey((currentState.prey.location._1, (currentState.prey.location._2 - cellSize) max 0)))
+            case KeyCode.DOWN => initialState.value = currentState.copy(prey = Prey((currentState.prey.location._1, (currentState.prey.location._2 + cellSize) min (windowSize - cellSize))))
+            case KeyCode.RIGHT => initialState.value = currentState.copy(prey = Prey(((currentState.prey.location._1 + cellSize) min (windowSize - cellSize), currentState.prey.location._2)))
+            case KeyCode.LEFT => initialState.value = currentState.copy(prey = Prey(((currentState.prey.location._1 - cellSize) max 0, currentState.prey.location._2)))
+            case _ => ()
+          }
+          content = drawState(initialState.value) // Actualise l'affichage après le déplacement de la proie
+        }
+      }
+    }
+
+    gameLoop { () =>
+      val currentState = initialState.value
+      initialState.value = updateGame(currentState)
+      Platform.runLater {
+        stage.getScene.content = drawState(initialState.value) // Actualise l'affichage après chaque mise à jour du jeu
+      }
+    }
+  }
+
+  def gameLoop(update: () => Unit): Unit = Future {
+    update()
+    Thread.sleep(100)
+    gameLoop(update)
+  }
+
+  def updateGame(state: State): State = {
+    // Implement movePredators to move each predator towards the prey
+    val updatedPredators = movePredators(state.predators, state.prey)
+    state.copy(predators = updatedPredators)
+  }
+
+  def movePredators(predators: List[Predator], prey: Prey): List[Predator] = {
+    predators.map { predator =>
+      val predatorLocation = predator.location
+      val preyLocation = prey.location
+
+      // Calculer les nouvelles coordonnées
+      val newX = if (preyLocation._1 > predatorLocation._1) predatorLocation._1 + 1 else if (preyLocation._1 < predatorLocation._1) predatorLocation._1 - 1 else predatorLocation._1
+      val newY = if (preyLocation._2 > predatorLocation._2) predatorLocation._2 + 1 else if (preyLocation._2 < predatorLocation._2) predatorLocation._2 - 1 else predatorLocation._2
+
+      // S'assurer que les nouvelles coordonnées sont dans les limites de la fenêtre
+      val boundedX = newX.max(0).min(windowSize - cellSize)
+      val boundedY = newY.max(0).min(windowSize - cellSize)
+
+      predator.copy(location = (boundedX, boundedY))
+    }
+  }
+
+
+  def drawState(state: State): List[Rectangle] = {
+    val prey = new Rectangle {
+      x = state.prey.location._1
+      y = state.prey.location._2
+      width = cellSize
+      height = cellSize
+      fill = Blue
+    }
+
+    val predators = state.predators.map { predator =>
+      new Rectangle {
+        x = predator.location._1
+        y = predator.location._2
+        width = cellSize
+        height = cellSize
+        fill = Red
+      }
+    }
+
+    prey :: predators
+  }
+}
